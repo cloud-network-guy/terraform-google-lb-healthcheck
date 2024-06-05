81ea41746db4bfab693594909da496f8dc361f72
jheyer <jheyer@opentext.com>
2024-06-05 12:02:41 -0700

    upgrade versions

--- versions.tf
+++ versions.tf
@@ -1,12 +1,16 @@
 terraform {
-  required_version = ">= 1.3.4"
+  required_version = ">= 1.5.7"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 5.16.0, <= 5.27.0"
+      version = ">= 5.16.0, <= 6.0"
     }
     random = {
       source  = "hashicorp/random"
+      version = ">= 3.4.0"
+    }
+    null = {
+      source  = "hashicorp/null"
       version = ">= 3.1.0"
     }
   }
